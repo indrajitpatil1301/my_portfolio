c53c9bbc3121c0e23a25fd7f08cdfe4f72728026
Admin <smbarge@gmail.com>
2024-11-13 12:20:30 +0530

    changes in package

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-    "./node_modules/flowbite/**/*.js",
+    // "./node_modules/flowbite/**/*.js",
+    "./src/**/*.{js,jsx,ts,tsx}", // Modify this pattern as needed based on your project structure
+
     // other paths
   ],
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  // purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {},
