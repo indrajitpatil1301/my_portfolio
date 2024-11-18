5fb78ca05f075dfa5e1ea524b2a1da51ef76b802
Admin <smbarge@gmail.com>
2024-11-18 12:16:32 +0530

    changes in project component changed the project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,7 +2,10 @@ import React from "react";
 
 const Projects = () => {
   return (
-    <section id="projects" className=" p-6 bg-gray-50 dark:bg-gray-900 transition-colors duration-300">
+    <section
+      id="projects"
+      className=" p-6 bg-gray-50 dark:bg-gray-900 transition-colors duration-300"
+    >
       <h2 className="text-3xl font-extrabold text-gray-800 dark:text-white text-center mb-6">
         Projects
       </h2>
@@ -10,10 +13,12 @@ const Projects = () => {
         {/* Project 1 */}
         <div className="p-6 bg-white shadow-lg rounded-lg hover:shadow-2xl transition-all duration-300 transform hover:scale-105 dark:bg-gray-800 dark:text-white">
           <h3 className="text-2xl font-semibold text-gray-800 dark:text-white">
-            Cmet-rs Recruitment Application
+            Central Gov Recruitment Application
           </h3>
           <p className="mt-4 text-gray-600 dark:text-gray-300">
-            This platform developed the application form for cmet-rs recruitment. The application used SvelteKit as the frontend and Node.js/Fastify as the backend stateless server.
+            This platform developed the application form for cmet-rs
+            recruitment. The application used SvelteKit as the frontend and
+            Node.js/Fastify as the backend stateless server.
           </p>
           <div className="mt-6">
             <span className="block text-gray-700 font-bold uppercase dark:text-gray-400">
@@ -28,10 +33,12 @@ const Projects = () => {
         {/* Project 2 */}
         <div className="p-6 bg-white shadow-lg rounded-lg hover:shadow-2xl transition-all duration-300 transform hover:scale-105 dark:bg-gray-800 dark:text-white">
           <h3 className="text-2xl font-semibold text-gray-800 dark:text-white">
-            KCB Recruitment Application
+            State Gov Recruitment Application
           </h3>
           <p className="mt-4 text-gray-600 dark:text-gray-300">
-            This platform developed the application form for KCB recruitment. The application used SvelteKit as the frontend and Node.js/Fastify as the backend stateless server.
+            This platform developed the application form for KCB recruitment.
+            The application used SvelteKit as the frontend and Node.js/Fastify
+            as the backend stateless server.
           </p>
           <div className="mt-6">
             <span className="block text-gray-700 font-bold uppercase dark:text-gray-400">
@@ -46,17 +53,56 @@ const Projects = () => {
         {/* Project 3 */}
         <div className="p-6 bg-white shadow-lg rounded-lg hover:shadow-2xl transition-all duration-300 transform hover:scale-105 dark:bg-gray-800 dark:text-white">
           <h3 className="text-2xl font-semibold text-gray-800 dark:text-white">
-            Movano Inc Admin Portal
+            Admin Portal for (Oversease Project)
           </h3>
           <p className="mt-4 text-gray-600 dark:text-gray-300">
-            The application manages Movano ring devices, partners, firmware, and app versions. It uses React and Redux for the frontend, and Node.js/Fastify for the backend.
+            The application manages Movano ring devices, partners, firmware, and
+            app versions. It uses React and Redux for the frontend, and
+            Node.js/Fastify for the backend.
           </p>
           <div className="mt-6">
             <span className="block text-gray-700 font-bold uppercase dark:text-gray-400">
               Technology Stack:
             </span>
             <span className="text-gray-600 dark:text-gray-300">
+              SvelteKit, TailwindCSS, Node.js, Fastify, MongoDB
+            </span>
               React, Redux, Node.js, Fastify, DynamoDB, AWS, CoreUI, Bootstrap
+          </div>
+        </div>
+
+        <div className="p-6 bg-white shadow-lg rounded-lg hover:shadow-2xl transition-all duration-300 transform hover:scale-105 dark:bg-gray-800 dark:text-white">
+          <h3 className="text-2xl font-semibold text-gray-800 dark:text-white">
+            SuperCraft (Billing Management)
+          </h3>
+          <p className="mt-4 text-gray-600 dark:text-gray-300">
+            This platform developed the application for Billing Management. The
+            application used SvelteKit as the frontend and Node.js/Fastify as
+            the backend.
+          </p>
+          <div className="mt-6">
+            <span className="block text-gray-700 font-bold uppercase dark:text-gray-400">
+              Technology Stack:
+            </span>
+            <span className="text-gray-600 dark:text-gray-300">
+              SvelteKit, TailwindCSS, Node.js, Fastify, MongoDB
+            </span>
+          </div>
+        </div>
+        <div className="p-6 bg-white shadow-lg rounded-lg hover:shadow-2xl transition-all duration-300 transform hover:scale-105 dark:bg-gray-800 dark:text-white">
+          <h3 className="text-2xl font-semibold text-gray-800 dark:text-white">
+            Think Creation (Passion Project)
+          </h3>
+          <p className="mt-4 text-gray-600 dark:text-gray-300">
+            This Platform developement is in Progress,
+            This Project is for Social Media Marketing 
+          </p>
+          <div className="mt-6">
+            <span className="block text-gray-700 font-bold uppercase dark:text-gray-400">
+              Technology Stack:
+            </span>
+            <span className="text-gray-600 dark:text-gray-300">
+              ReactJs, TailwindCSS, Node.js, Express, MongoDB
             </span>
           </div>
         </div>
